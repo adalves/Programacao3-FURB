ae6068b41488a82c5a452af1b40582c3c7d11d8a
Ana <acrsaw@gmail.com>
2020-07-14 07:15:26 -0300

    api

---
+++ Trabalho-final/pokemonAPI/Persistence/MariaDB/PokemonContext.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using pokemonAPI.Domain.Models;
+
+namespace pokemonAPI.Persistence.MariaDB
+{
+    public partial class PokemonContext : DbContext
+    {
+        public PokemonContext()
+        {
+        }
+
+        public PokemonContext(DbContextOptions<PokemonContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ListPokemon> ListPokemon { get; set; }
+        public virtual DbSet<Moves> Moves { get; set; }
+        public virtual DbSet<Pokemon> Pokemon { get; set; }
+        public virtual DbSet<PokemonMoves> PokemonMoves { get; set; }
+        public virtual DbSet<TypeEffectiveness> TypeEffectiveness { get; set; }
+        public virtual DbSet<Types> Types { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ListPokemon>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToView("list_pokemon");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnType("varchar(50)")
+                    .HasDefaultValueSql("''")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.Type1)
+                    .HasColumnType("varchar(50)")
+                    .HasDefaultValueSql("''")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.Type2)
+                    .HasColumnType("varchar(50)")
+                    .HasDefaultValueSql("''")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e._)
+                    .HasColumnName("#")
+                    .HasColumnType("int(11)");
+            });
+
+            modelBuilder.Entity<Moves>(entity =>
+            {
+                entity.HasKey(e => e.CdMove)
+                    .HasName("PRIMARY");
+
+                entity.ToTable("moves");
+
+                entity.HasIndex(e => e.CdType)
+                    .HasName("cd_type_fk");
+
+                entity.Property(e => e.CdMove)
+                    .HasColumnName("cd_move")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.CdType)
+                    .HasColumnName("cd_type")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.NmCategory)
+                    .IsRequired()
+                    .HasColumnName("nm_category")
+                    .HasColumnType("varchar(50)")
+                    .HasDefaultValueSql("''")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.NmMove)
+                    .IsRequired()
+                    .HasColumnName("nm_move")
+                    .HasColumnType("varchar(50)")
+                    .HasDefaultValueSql("''")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.NrAcc)
+                    .HasColumnName("nr_acc")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.NrPp)
+                    .HasColumnName("nr_pp")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.NrPwr)
+                    .HasColumnName("nr_pwr")
+                    .HasColumnType("int(11)");
+
+                entity.HasOne(d => d.CdTypeNavigation)
+                    .WithMany(p => p.Moves)
+                    .HasForeignKey(d => d.CdType)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("cd_type_fk");
+            });
+
+            modelBuilder.Entity<Pokemon>(entity =>
+            {
+                entity.HasKey(e => e.CdPoke)
+                    .HasName("PRIMARY");
+
+                entity.ToTable("pokemon");
+
+                entity.HasIndex(e => e.CdType1)
+                    .HasName("type1_fk");
+
+                entity.HasIndex(e => e.CdType2)
+                    .HasName("type2_fk");
+
+                entity.HasIndex(e => e.NmPoke)
+                    .HasName("nm_poke");
+
+                entity.Property(e => e.CdPoke)
+                    .HasColumnName("cd_poke")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.CdType1)
+                    .HasColumnName("cd_type1")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.CdType2)
+                    .HasColumnName("cd_type2")
+                    .HasColumnType("int(11)")
+                    .HasDefaultValueSql("'19'");
+
+                entity.Property(e => e.NmPoke)
+                    .IsRequired()
+                    .HasColumnName("nm_poke")
+                    .HasColumnType("varchar(50)")
+                    .HasDefaultValueSql("''")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.HasOne(d => d.CdType1Navigation)
+                    .WithMany(p => p.PokemonCdType1Navigation)
+                    .HasForeignKey(d => d.CdType1)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("type1_fk");
+
+                entity.HasOne(d => d.CdType2Navigation)
+                    .WithMany(p => p.PokemonCdType2Navigation)
+                    .HasForeignKey(d => d.CdType2)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("type2_fk");
+            });
+
+            modelBuilder.Entity<PokemonMoves>(entity =>
+            {                
+                entity.HasKey(e => new {e.CdMove, e.CdPoke});
+
+                entity.ToTable("pokemon_moves");
+
+                entity.HasIndex(e => e.CdMove)
+                    .HasName("cd_move_fk");
+
+                entity.HasIndex(e => e.CdPoke)
+                    .HasName("cd_poke_fk");
+
+                entity.Property(e => e.CdMove)
+                    .HasColumnName("cd_move")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.CdPoke)
+                    .HasColumnName("cd_poke")
+                    .HasColumnType("int(11)");
+
+                entity.HasOne(d => d.CdMoveNavigation)
+                    .WithMany()
+                    .HasForeignKey(d => d.CdMove)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("cd_move_fk");
+
+                entity.HasOne(d => d.CdPokeNavigation)
+                    .WithMany()
+                    .HasForeignKey(d => d.CdPoke)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("cd_poke_fk");
+            });
+
+            modelBuilder.Entity<TypeEffectiveness>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("type_effectiveness");
+
+                entity.HasIndex(e => e.CdAtkType)
+                    .HasName("atk_type_fk");
+
+                entity.HasIndex(e => e.CdDefType)
+                    .HasName("def_type_fk");
+
+                entity.Property(e => e.CdAtkType)
+                    .HasColumnName("cd_atk_type")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.CdDefType)
+                    .HasColumnName("cd_def_type")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.NrEffectiveness)
+                    .HasColumnName("nr_effectiveness")
+                    .HasColumnType("decimal(2,1)")
+                    .HasDefaultValueSql("'1.0'");
+
+                entity.HasOne(d => d.CdAtkTypeNavigation)
+                    .WithMany()
+                    .HasForeignKey(d => d.CdAtkType)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("atk_type_fk");
+
+                entity.HasOne(d => d.CdDefTypeNavigation)
+                    .WithMany()
+                    .HasForeignKey(d => d.CdDefType)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("def_type_fk");
+            });
+
+            modelBuilder.Entity<Types>(entity =>
+            {
+                entity.HasKey(e => e.CdType)
+                    .HasName("PRIMARY");
+
+                entity.ToTable("types");
+
+                entity.Property(e => e.CdType)
+                    .HasColumnName("cd_type")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.NmType)
+                    .IsRequired()
+                    .HasColumnName("nm_type")
+                    .HasColumnType("varchar(50)")
+                    .HasDefaultValueSql("''")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Trabalho-final/pokemonAPI/Persistence/MariaDB/PokemonRepository.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using pokemonAPI.Domain.Interfaces;
+using pokemonAPI.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace pokemonAPI.Persistence.MariaDB
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly PokemonContext context;
+        public PokemonRepository(PokemonContext context) {
+            this.context = context;
+        }
+
+        public List<PokemonDTO> ListAll()
+        {
+            var pokemon = (from obj in context.Pokemon
+                            .Include(p => p.CdType1Navigation)
+                            .Include(p => p.CdType2Navigation)
+                            .OrderBy(x => x.CdPoke)
+                            select new PokemonDTO()
+                            {
+                                ID = obj.CdPoke.ToString(),
+                                Name = obj.NmPoke,
+                                Type1 = obj.CdType1Navigation.NmType,
+                                Type2 = obj.CdType2Navigation.NmType
+                            }).ToList();
+            return (List<PokemonDTO>)pokemon;
+        }
+
+        public PokemonDTO GetByID(string id)
+        {
+            var pokemon = (from obj in context.Pokemon
+                            .Include(p => p.CdType1Navigation)
+                            .Include(p => p.CdType2Navigation)
+                            select new PokemonDTO()
+                            {
+                                ID = obj.CdPoke.ToString(),
+                                Name = obj.NmPoke,
+                                Type1 = obj.CdType1Navigation.NmType,
+                                Type2 = obj.CdType2Navigation.NmType
+                            }).SingleOrDefault(x => x.ID.Equals(id));
+            return pokemon;
+        }
+
+        public void Add(PokemonDTO pokemon)
+        {
+
+            Types type1 = context.Types.SingleOrDefault(t => t.NmType == pokemon.Type1);
+            Types type2 = context.Types.SingleOrDefault(t => t.NmType == pokemon.Type2);
+
+            Pokemon newPoke = new Pokemon() { CdPoke = int.Parse(pokemon.ID), NmPoke = pokemon.Name, 
+                                            CdType1 = type1.CdType, CdType2 = type2.CdType,
+                                            CdType1Navigation = type1, CdType2Navigation = type2 };
+
+            context.Pokemon.Add(newPoke);
+            context.SaveChanges();
+        }
+
+        public void Update(PokemonDTO pokemon)
+        {
+            var persisted = context.Pokemon.SingleOrDefault(x => x.CdPoke.ToString().Equals(pokemon.ID));
+            if (persisted == null)
+                return;
+            
+            persisted.NmPoke = pokemon.Name;
+
+            var type1 = context.Types.SingleOrDefault(t => t.NmType == pokemon.Type1);
+            var type2 = context.Types.SingleOrDefault(t => t.NmType == pokemon.Type2);
+
+            persisted.CdType1 = type1.CdType;
+            persisted.CdType2 = type2.CdType;
+            persisted.CdType1Navigation = type1;
+            persisted.CdType2Navigation = type2;
+
+            context.SaveChanges();
+        }
+
+        public void DeleteByID(string id)
+        {
+            var pokemon = context.Pokemon.SingleOrDefault(x => x.CdPoke.ToString().Equals(id));
+            if (pokemon == null)
+                return;
+            List<MovesDTO> moves = ListMoves(id);
+            foreach(MovesDTO move in moves) {
+                DeleteMove(move.Name, id);
+            }
+            context.Pokemon.Remove(pokemon);
+            context.SaveChanges();
+        }
+        
+        public MovesDTO GetByName(string name) {
+            var move = (from obj in context.Moves
+                            .Include(m => m.CdTypeNavigation)
+                            select new MovesDTO()
+                            {
+                                Name = obj.NmMove,
+                                Type = obj.CdTypeNavigation.NmType,
+                                Category = obj.NmCategory,
+                                Power = obj.NrPwr.ToString(),
+                                Accuracy = obj.NrAcc.ToString(),
+                                PP = obj.NrPp.ToString()
+                            }).SingleOrDefault(obj => obj.Name.Equals(name));
+            return move;
+        }
+        public List<MovesDTO> ListMoves(string id)
+        {
+            var moves = (from obj in context.PokemonMoves
+                            .Include(pm => pm.CdMoveNavigation)
+                            .Include(pm => pm.CdPokeNavigation)
+                            .Include(pm => pm.CdMoveNavigation.CdTypeNavigation)
+                            .Where (pm => pm.CdPoke.ToString().Equals(id))
+                            select new MovesDTO()
+                            {
+                                Name = obj.CdMoveNavigation.NmMove,
+                                Type = obj.CdMoveNavigation.CdTypeNavigation.NmType,
+                                Category = obj.CdMoveNavigation.NmCategory,
+                                Power = obj.CdMoveNavigation.NrPwr.ToString(),
+                                Accuracy = obj.CdMoveNavigation.NrAcc.ToString(),
+                                PP = obj.CdMoveNavigation.NrPp.ToString()
+                            }).ToList();
+            return (List<MovesDTO>)moves;
+        }
+
+        public void AddMove(string nameMove, string idPkmn)
+        {
+            Pokemon pokemon = context.Pokemon.SingleOrDefault(p => p.CdPoke.ToString().Equals(idPkmn));
+            Moves move = context.Moves.SingleOrDefault(m => m.NmMove.Equals(nameMove));
+
+            PokemonMoves pkmnMove = new PokemonMoves() { CdPoke = pokemon.CdPoke, CdMove = move.CdMove,
+                                                        CdMoveNavigation = move, CdPokeNavigation = pokemon };
+            
+            context.PokemonMoves.Add(pkmnMove);
+            context.SaveChanges();
+        }
+
+        public void DeleteMove(string nameMove, string idPkmn)
+        {            
+            Moves move = context.Moves.SingleOrDefault(m => m.NmMove.Equals(nameMove));
+
+            var pkmnMove = context.PokemonMoves.SingleOrDefault(p => (p.CdMove == move.CdMove && p.CdPoke == int.Parse(idPkmn)));
+            
+            context.PokemonMoves.Remove(pkmnMove);
+            context.SaveChanges();
+        }
+        
+        public Dictionary<string,string> ListTypes(string id) 
+        {
+            Pokemon pokemon = context.Pokemon.SingleOrDefault(p => p.CdPoke.ToString().Equals(id));
+
+            var type1 = (from obj in context.TypeEffectiveness
+                            .Include(te => te.CdAtkTypeNavigation)
+                            .Include(te => te.CdDefTypeNavigation)
+                            .Where (te => (te.CdDefType == pokemon.CdType1 && te.CdAtkType != 19))
+                            select new EffectivenessDTO()
+                            {
+                                Type = obj.CdAtkTypeNavigation.NmType,
+                                Effectiveness = (decimal)obj.NrEffectiveness
+                            }).ToList();
+                            
+            var type2 = (from obj in context.TypeEffectiveness
+                            .Include(te => te.CdAtkTypeNavigation)
+                            .Include(te => te.CdDefTypeNavigation)
+                            .Where (te => (te.CdDefType == pokemon.CdType2 && te.CdAtkType != 19))
+                            select new EffectivenessDTO()
+                            {
+                                Type = obj.CdAtkTypeNavigation.NmType,
+                                Effectiveness = (decimal)obj.NrEffectiveness
+                            }).ToList();
+
+            Dictionary<string,string> result = new Dictionary<string,string>();
+            for (int i = 0; i < type1.Count(); ++i) {
+                result.Add(type1[i].Type, (type1[i].Effectiveness * type2[i].Effectiveness).ToString());
+            }
+
+            return result;
+        }
+    }
+}
